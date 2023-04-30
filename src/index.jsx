777a4dcab5a34623c4efb255ca4b0f9f7632b891
TakushiTokuyama <sokoninoruna@gmail.com>
2023-04-30 12:15:26 +0900

    削除

--- src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { TodoPage } from "./pages/TodoPage";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<TodoPage />);
